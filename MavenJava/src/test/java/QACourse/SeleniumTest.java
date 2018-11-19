f06a0a24bf0521c32f431d4db45fb40b09be517f
azimmalik9 <azim.malik9@gmail.com>
2018-11-19 12:14:46 +0000

    add surefire to execute tests

---
+++ MavenJava/src/test/java/QACourse/SeleniumTest.java
@@ -0,0 +1,16 @@
+package QACourse;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementsUI() {
+		System.out.println("elementsUI");
+	}
+}
